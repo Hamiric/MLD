9b60ec32fc824cb97aa4db6e9b12446da095dde8
lhj4499 <ucs210212@naver.com>
2021-10-05 17:33:02 +0900

    프로젝트 UI-이학준

---
+++ MyLittleDoctor/app/src/main/java/com/example/mylittledoctor/HospitalMap.java
@@ -0,0 +1,14 @@
+package com.example.mylittledoctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HospitalMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital_map);
+    }
+}
