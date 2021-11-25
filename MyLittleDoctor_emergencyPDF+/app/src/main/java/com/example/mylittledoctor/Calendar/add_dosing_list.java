927115932940ff4e88376ae721c0ff93b6a80537
Hamiric <do9870@naver.com>
2021-11-25 09:35:08 +0900

    1차 액티비티 연결에 응급상식 pdf추가 + 알람 추가

--- MyLittleDoctor_emergencyPDF+/app/src/main/java/com/example/mylittledoctor/Calendar/add_dosing_list.java
+++ MyLittleDoctor_emergencyPDF+/app/src/main/java/com/example/mylittledoctor/Calendar/add_dosing_list.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -13,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mylittledoctor.R;
+
+import java.util.ArrayList;
 
 public class add_dosing_list extends AppCompatActivity {
 
@@ -22,12 +25,16 @@ public class add_dosing_list extends AppCompatActivity {
     private ImageButton btn_finish;
     private Button btn_add;
     private TextView adding_request;
+    private LinearLayout alram;
+
+    ArrayList<String> title = new ArrayList<String>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_add_dosing_list);
 
+        alram = (LinearLayout) findViewById(R.id.alram_setting);
         btn_finish = (ImageButton) findViewById(R.id.imageButton3);
         btn_add = (Button) findViewById(R.id.add_item_dosing_list);
         listView = (ListView) findViewById(R.id.listview_item_dosing);
@@ -35,6 +42,18 @@ public class add_dosing_list extends AppCompatActivity {
 
         adapter = new ListViewAdapter_dosing(add_dosing_list.this);
         listView.setAdapter(adapter);
+
+        alram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AlramActivity.class);
+                for(int i = 0 ; i < title.size() ; i ++) {
+                    intent.putStringArrayListExtra("title", title);
+                }
+                setResult(RESULT_OK, intent);
+                startActivity(intent);
+            }
+        });
 
         btn_finish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,9 +77,8 @@ public class add_dosing_list extends AppCompatActivity {
         if(requestCode==1){
             if(resultCode==RESULT_OK){
                 //데이터 받기
-                String title = data.getStringExtra("title");
-                Log.d("title", "onActivityResult: " + title);
-                adapter.addItem(title);
+                title.add(data.getStringExtra("title"));
+                adapter.addItem(data.getStringExtra("title"));
 
                 adding_request.setVisibility(View.GONE);
 
